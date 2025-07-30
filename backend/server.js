f7c1b58664cb5622a3ff5b5e6d560b6adacbf329
Coldz-21 <michaelgoddard.buenaflor@gmail.com>
2025-07-30 11:53:48 -0400

    Revised frontend
    Changes a lot

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,6 @@ app.get('*', (req, res) => {
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
